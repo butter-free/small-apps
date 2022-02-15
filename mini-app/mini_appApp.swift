ab3d3fe547815769c31ee0bd5f7e6d10863b7ec0
butter-free <butterfree@kakao.com>
2022-02-16 00:18:45 +0900

    Initial Commit

---
+++ mini-app/mini_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mini_appApp.swift
+//  mini-app
+//
+//  Created by sean on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct mini_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
