a8c4b0a0c5b64cf9f0ce2929a469e2aae02041cf
butter-free <butterfree@kakao.com>
2022-04-27 23:38:13 +0900

    feat: SearchListView store 적용
     - Store 적용으로 Local State items -> viewStore searchItemList 변경
     - SearchList Tap시 repository 웹 페이지 처리를 위해 SafariServiceView 사용

--- TCA/TCA/Presentation/MainView.swift
+++ TCA/TCA/Presentation/MainView.swift
@@ -5,48 +5,61 @@
 //  Created by sean on 2022/03/08.
 //
 
+import Combine
 import SwiftUI
 
 struct MainView: View {
   
-  @State private var text: String = ""
-  
   enum Height {
-    static let tabBar: CGFloat = UIDevice.current.hasNotch ? 110 : 80
+    static let tabBar: CGFloat = UIDevice.current.hasNotch ? 80 : 50
   }
   
+  @State private var text: String = ""
+  
   init() {
-    UITabBar.appearance().barTintColor = .systemBackground
+    UITabBar.appearance().barTintColor = .white
     UITabBar.appearance().frame.size.height = Height.tabBar
     UITabBar.appearance().frame.origin.y = UIScreen.main.bounds.height - Height.tabBar
   }
   
   var body: some View {
     TabView {
-      SearchListView()
-        .searchable(text: $text)
-        .tabItem {
-          Image(systemName: "flame.fill")
-        }
+      SearchListView(
+        store: .init(
+          initialState: SearchListState(),
+          reducer: searchListReducer,
+          environment: SearchListEnvironment(
+            searchUseCase: SearchDefaultUseCase(
+              repository: SearchDataRepository()
+            ),
+            mainQueue: DispatchQueue.main.eraseToAnyScheduler()
+          )
+        )
+      )
+      .searchable(text: $text)
+      .padding(.init(top: 0, leading: 0, bottom: 20, trailing: 0))
+      .tabItem {
+        Image(systemName: "flame.fill")
+      }
       Text("Second")
         .tabItem {
           Image(systemName: "star")
         }
-      Text("Second")
+      Text("Third")
         .tabItem {
           Image(systemName: "person")
         }
-      Text("Second")
+      Text("Fourth")
         .tabItem {
           Image(systemName: "gearshape")
         }
     }
+    .background(Color.white)
   }
 }
 
 struct MainView_Previews: PreviewProvider {
   static var previews: some View {
     MainView()
-      
   }
 }
--- TCA/TCA/Presentation/SearchList/SearchListView.swift
+++ TCA/TCA/Presentation/SearchList/SearchListView.swift
@@ -7,30 +7,76 @@
 
 import SwiftUI
 
-  }
-}
+import ComposableArchitecture
 
 struct SearchListView: View {
   
+  enum Height {
+    static let itemView: CGFloat = 130
+  }
+  
   @State var searchedText: String = ""
-  @State private var items: [SearchItem] = [.init(), .init()]
+  @State var isOpenSafari: Bool = false
+  
+  let store: Store<SearchListState, SearchListAction>
   
   var body: some View {
-    VStack(spacing: 0) {
-      SearchBar(searchText: searchedText)
-      ScrollView(.vertical, showsIndicators: false) {
-        ForEach(items, id: \.id) { item in
-          SearchItemView(item: item) {
-            print("tap")
-          }.frame(width: .infinity, height: 160)
+    WithViewStore(store) { viewStore in
+      GeometryReader { geometry in
+        Color.white
+        VStack(spacing: 0) {
+          SearchBar(searchText: searchedText)
+          Divider()
+          List {
+            ForEach(Array(viewStore.searchItemList.enumerated()), id: \.offset) { index, item in
+              SearchItemView(
+                item: item,
+                didTap: {
+                  self.isOpenSafari = true
+                },
+                didTapStarButton: {
+                  print("tap star button")
+                }
+              )
+              .listRowBackground(Color.white)
+              .listRowSeparator(.hidden)
+              .edgesIgnoringSafeArea(.all)
+              .frame(height: Height.itemView)
+              .sheet(isPresented: $isOpenSafari, onDismiss: dismissSheet) {
+                if let url = URL(string: item.repositoryURLString) {
+                  SafariServiceView(url: url)
+                }
+              }
+            }
+          }
+          .background(Color.white)
+          .listStyle(PlainListStyle())
+          .edgesIgnoringSafeArea(.all)
+          .frame(maxWidth: .infinity)
         }
+      }.onAppear {
+        viewStore.send(SearchListAction.requestSearchItemList("swift"))
       }
     }
+  }
+  
+  func dismissSheet() {
+    isOpenSafari = false
   }
 }
 
 struct SearchListView_Previews: PreviewProvider {
   static var previews: some View {
-    SearchListView()
+    SearchListView(
+      store: Store(
+        initialState: SearchListState(),
+        reducer: searchListReducer,
+        environment: SearchListEnvironment(
+          searchUseCase: SearchDefaultUseCase(
+            repository: SearchDataRepository()
+          ),
+          mainQueue: DispatchQueue.main.eraseToAnyScheduler())
+      )
+    )
   }
 }
