36a21b7f9dc311dd3cfc484d1518c07f73d72b8d
butter-free <butterfree@kakao.com>
2022-05-10 23:26:35 +0900

    build: Tuist Project 및 Dependency 설정 코드 추가
     프로젝트 설정을 위한 파일 및 코드 추가

---
+++ TCA/Project.swift
@@ -0,0 +1,84 @@
+//
+//  Project.swift
+//  TCA
+//
+//  Created by sean on 2022/05/06.
+//
+
+import ProjectDescription
+
+let projectName: String = "TCA"
+let organizationName: String = "sample"
+let bundleName: String = "com.sample"
+
+let baseSetting: [String: SettingValue] = [
+  "MARKETING_VERSION": "1.0.0",
+  "PRODUCT_MODULE_NAME": "\(projectName)"
+]
+
+// info.plist의 내용을 직접 지정
+// TODO: - Build version auto increment & template
+let infoPlist: [String: InfoPlist.Value] = [
+  "CFBundleName": "\(projectName)",
+  "CFBundleDisplayName": "\(projectName)",
+  "CFBundleIdentifier": "\(bundleName).\(projectName)",
+  "CFBundleShortVersionString": "1.0",
+  "CFBundleVersion": "0",
+  "CFBuildVersion": "0",
+  "UILaunchStoryboardName": "LaunchScreen",
+  "UISupportedInterfaceOrientations" : ["UIInterfaceOrientationPortrait"],
+  "UIUserInterfaceStyle":"Light"
+]
+
+let settings: Settings = .settings(
+  base: baseSetting,
+  configurations: [
+    .release(name: .release)
+  ],
+  defaultSettings: .recommended
+)
+
+let targets = [
+  Target(
+    name: projectName,
+    platform: .iOS,
+    product: .app,
+    bundleId: "\(bundleName).\(projectName)",
+    deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: "\(projectName)/Sources/**",
+    resources: "\(projectName)/Resources/**",
+    dependencies: [
+      .external(name: "ComposableArchitecture"),
+      .external(name: "FirebaseAuth"),
+      .external(name: "FirebaseAuthCombine-Community")
+    ],
+    settings: settings
+  ),
+  Target( /// unit test
+    name: "\(projectName)Tests",
+    platform: .iOS,
+    product: .unitTests,
+    bundleId: "\(bundleName).\(projectName)Tests",
+    sources: "\(projectName)Tests/**",
+    dependencies: [
+      .target(name: projectName) /// 테스트의 의존성은 실제 프로젝트에 있음
+    ]
+        ),
+  Target(
+    name: "\(projectName)UITests",
+    platform: .iOS,
+    product: .uiTests,
+    bundleId: "\(bundleName).\(projectName)UITests",
+    sources: "\(projectName)UITests/**",
+    dependencies: [
+      .target(name: projectName)
+    ]
+  )
+]
+
+let project = Project(
+  name: projectName,
+  organizationName: organizationName,
+  targets: targets
+)
