fd6a3b6b3d795fa255818c816af2cc05dff00d18
butter-free <butterfree@kakao.com>
2022-05-16 12:11:00 +0900

    feat, refactor: MainView 탭바 설정 변경, 변경 화면요소 적용
     - Tabbar 상단 separator 미사용으로 코드 추가
     - 공용 사용을 위한 코드 적용으로 불필요 코드 제거 진행

--- TCA/TCA/Sources/Presentation/MainView.swift
+++ TCA/TCA/Sources/Presentation/MainView.swift
@@ -16,31 +16,21 @@ struct MainView: View {
   }
   
   init() {
-    UITabBar.appearance().barTintColor = .white
+    let appearance = UITabBarAppearance()
+    appearance.backgroundColor = .white
+    appearance.shadowImage = nil
+    appearance.shadowColor = nil
+    UITabBar.appearance().standardAppearance = appearance
   }
   
   var body: some View {
     TabView {
-      SearchListView(
-        store: .init(
-          initialState: SearchListState(),
-          reducer: searchListReducer,
-          environment: SearchListEnvironment(
-            searchUseCase: SearchDefaultUseCase(
-              repository: SearchDataRepository()
-            ),
-            mainQueue: DispatchQueue.main.eraseToAnyScheduler()
-          )
-        )
-      )
-      .edgesIgnoringSafeArea(.bottom)
-      .padding(.init(top: 0, leading: 0, bottom: Padding.SearchListViewBottom, trailing: 0))
-      .tabItem {
-        Image(systemName: "flame.fill")
-      }
-      Text("Second")
+      SearchListView()
+        .padding(.init(top: 0, leading: 0, bottom: Padding.SearchListViewBottom, trailing: 0))
+        .tabItem {
+          Image(systemName: "flame.fill")
+        }
       StarredListView()
-        .edgesIgnoringSafeArea(.bottom)
         .padding(.init(top: 0, leading: 0, bottom: Padding.StarredListViewBottom, trailing: 0))
         .tabItem {
           Image(systemName: "star")
@@ -61,6 +51,5 @@ struct MainView: View {
 struct MainView_Previews: PreviewProvider {
   static var previews: some View {
     MainView()
-      
   }
 }
