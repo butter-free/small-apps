2f061994d06adc6b4198b97378f03f5a53ec26e2
butter-free <butterfree@kakao.com>
2022-05-16 10:53:50 +0900

    docs, feat: UserInfoRepository 추가
     AccessToken을 이용해 유저 정보를 처리하기위해서 UserInfoRepository 추가 함

---
+++ TCA/TCA/Sources/Domain/Repository/UserInfoRepository.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfoRepository.swift
+//  TCA
+//
+//  Created by sean on 2022/05/14.
+//  Copyright © 2022 butterfree. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+protocol UserInfoRepository {
+  func requestUserInfo(accessToken: String) -> AnyPublisher<UserInfo, URLError>
+}
