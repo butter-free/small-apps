4cde7c7451999d959f73ca4566ea4e8871cc8822
butter-free <butterfree@kakao.com>
2022-05-24 14:43:47 +0900

    feat: 테스트용 ApiManagerStub 응답 실패시 처리추가 및 코드정리
     - 응답 실패테스트를 위해 hasOccerError 변수 추가하고 true로 설정되면 URLError(.cancelled) 반환하는 코드 추가
     - Endpoint path 분기 제거 모든 더미 api요청은 SampleData 정의대로만 반환하도록 수정

--- TCA/TCA/Sources/Network/Stub/ApiManagerStub.swift
+++ TCA/TCA/Sources/Network/Stub/ApiManagerStub.swift
@@ -9,24 +9,31 @@ import Combine
 import Foundation
 
 class ApiManagerStub: ApiService {
+  
+  let hasOccerError: Bool
+  
+  init(hasOccerError: Bool = false) {
+    self.hasOccerError = hasOccerError
+  }
+  
   func request<T>(endPoint: EndPoint) -> AnyPublisher<T, URLError> where T : Decodable {
     
-    switch endPoint.path {
-    case .search:
-      let data = SampleData(
-        path: endPoint.path
-      ).create()
-      return Just(data)
-        .decode(type: T.self, decoder: JSONDecoder()).eraseToAnyPublisher()
-        .mapError { error -> URLError in
-          if let error = error as? URLError {
-            return error
-          } else {
-            return URLError(.cannotDecodeRawData)
-          }
-        }.eraseToAnyPublisher()
-    default:
-      return Empty(completeImmediately: true).eraseToAnyPublisher()
+    if hasOccerError {
+      return Fail(error: URLError(.cancelled)).eraseToAnyPublisher()
     }
+    
+    let data = SampleData(
+      path: endPoint.path
+    ).create()
+    
+    return Just(data)
+      .decode(type: T.self, decoder: JSONDecoder()).eraseToAnyPublisher()
+      .mapError { error -> URLError in
+        if let error = error as? URLError {
+          return error
+        } else {
+          return URLError(.cannotDecodeRawData)
+        }
+      }.eraseToAnyPublisher()
   }
 }
