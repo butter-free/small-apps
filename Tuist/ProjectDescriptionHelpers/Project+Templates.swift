cd3816496cf214c4e2b05a7ea713dce3cd84d850
sean <butterfree@kakao.com>
2022-08-28 23:41:12 +0900

    build: Tuist Templates 포맷정리

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -8,31 +8,29 @@
 import ProjectDescription
 
 extension Project {
-  public static func featureFramework(
+  public static func feature(
     name: String,
     products: [Product],
-    settings: Settings? = nil,
+    settings: Settings? = .default,
     infoPlist: InfoPlist = .default,
     dependencies: [TargetDependency] = []
   ) -> Project {
     
     var targets: [Target] = []
-    
-    if products.contains(.app) {
-      let target: Target = .init(
-        name: name,
-        platform: .iOS,
-        product: .app,
-        bundleId: "com.butterfree.\(name)",
-        deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
-        infoPlist: infoPlist,
-        sources: ["Sources/**"],
-        resources: ["Resources/**"],
-        dependencies: dependencies,
-        settings: settings
-      )
-      targets.append(target)
-    }
+
+    let target: Target = .init(
+      name: name,
+      platform: .iOS,
+      product: products[0],
+      bundleId: "com.butterfree.\(name)",
+      deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+      infoPlist: infoPlist,
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: dependencies,
+      settings: settings
+    )
+    targets.append(target)
     
     if products.contains(.unitTests) {
       let target: Target = .init(
@@ -64,44 +62,5 @@ extension Project {
       name: name,
       targets: targets
     )
-  }
-  
-  public static func makeSettings() -> Settings {
-    
-    let baseSetting: [String: SettingValue] = [:]
-    
-    return .settings(
-      base: baseSetting,
-      configurations: [
-        .release(name: .release)
-      ],
-      defaultSettings: .recommended
-    )
-  }
-  
-  // info.plist의 내용을 직접 지정
-  public static func makeInfoPlist(
-    name: String,
-    bundleName: String = "com.butterfree"
-  ) -> [String: InfoPlist.Value] {
-    return [
-      "CFBundleName": .string(name),
-      "CFBundleDisplayName": .string(name),
-      "CFBundleIdentifier": .string("\(bundleName).sample"),
-      "CFBundleShortVersionString": .string("1.0"),
-      "CFBundleVersion": .string("0"),
-      "CFBuildVersion": .string("0"),
-      "CFBundleURLTypes": .array([
-        .dictionary([
-          "CFBundleTypeRole": .string("Editor"),
-          "CFBundleURLSchemes": .array([
-            .string("com.googleusercontent.apps.1035822977996-7r1up0avlf296ho78knjsqhbkf2s1994")
-          ])
-        ])
-      ]),
-      "UILaunchStoryboardName": .string("Launch Screen"),
-      "UISupportedInterfaceOrientations": .array([.string("UIInterfaceOrientationPortrait")]),
-      "UIUserInterfaceStyle": .string("Light")
-    ]
   }
 }
